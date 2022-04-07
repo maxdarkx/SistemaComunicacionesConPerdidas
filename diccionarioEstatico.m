e5996ab346adb746b8193dd542c2f13c813c4d16
Juan <jcarlos.maya@udea.edu.co>
2022-04-07 00:56:11 +0000

    se agregan los indices binarios al diccionario

--- diccionarioEstatico.m
+++ diccionarioEstatico.m
@@ -1,10 +1,12 @@
 function [diccionario] = diccionarioEstatico(data)
     tam= length(data);
+    indexBin = "0";
     diccionario = [dec2bin(0, 6), convertCharsToStrings(data(1))];
     for i=2:tam
         if ~contains(diccionario, data(i))
-            [string(i-1), dec2bin(i-1, 6)]
-            diccionario = cat(1, diccionario, [dec2bin(i-1, 6), convertCharsToStrings(data(i))]);
+            index = i-1; 
+            indexBin = dec2bin(index, 6);
+            diccionario = cat(1, diccionario, [indexBin, convertCharsToStrings(data(i))]);
         end
     end
 end
